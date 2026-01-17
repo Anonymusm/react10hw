bd1403327ec9c879cf2b59892eddde319cd4e03d
Anton <141570541+Anonymusm@users.noreply.github.com>
2026-01-17 20:05:00 +0200

    finished hw

--- vite-project/src/components/ContactContainer/ContactContainer.jsx
+++ vite-project/src/components/ContactContainer/ContactContainer.jsx
@@ -1,15 +1,28 @@
 import { Filter } from "../Filter/Filter";
 import { ContactList } from "../ContactList/ContactList";
 import ContactForm from "../ContactForm/ContactForm";
+import { useContext } from "react";
+import { Context } from "../ContactContext";
 
 export default function ContactContainer() {
+  const { contacts, filter } = useContext(Context);
+  const normalizedFilter = filter.toLowerCase();
+
+  const filtered = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(normalizedFilter),
+  );
   return (
     <>
       <h2>Phonebook</h2>
       <ContactForm />
 
-      <h2>Contacts</h2>
-      <Filter />
+      {contacts.length > 0 && (
+        <>
+          <h2>Contacts</h2>
+          <Filter />
+        </>
+      )}
+      {contacts.length > 0 && filtered.length === 0 && <p>No todos found!</p>}
       <ContactList />
     </>
   );
